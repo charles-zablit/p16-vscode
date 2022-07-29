a24adbd3fa815304e23976b133e4a11df571a1ac
robyzzz <robyawyx@gmail.com>
2022-07-29 18:44:58 +0100

    added main.ts

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import {
+  workspace as Workspace,
+  window,
+  ExtensionContext,
+  TextDocumentChangeEvent,
+} from "vscode";
+
+export function activate(context: ExtensionContext) {
+  console.log("haaa");
+
+  Workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
+    console.log("onDidChangeTextDocument");
+  });
+} 
